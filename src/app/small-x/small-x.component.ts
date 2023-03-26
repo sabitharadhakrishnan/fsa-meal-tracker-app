9f0c9fea49bf9a207b7aef00558340af5ceef32d
Sabitha Radhakrishnan <sabitharadhakrishnan@gmail.com>
2023-03-26 02:47:17 -0500

    pull code from template

---
+++ src/app/small-x/small-x.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-x',
+  templateUrl: './small-x.component.html',
+  styleUrls: ['./small-x.component.css']
+})
+export class SmallXComponent {
+
+}
